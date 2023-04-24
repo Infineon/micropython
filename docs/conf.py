2ec74ccc65a055846a395c150bcc13c891484307
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2023-04-24 14:26:36 +0200

    docs: Added docs and docs conf for PSoC6 port.
    - Added PSoC6 quick reference to docs.
    - Added sphinx-tabs as install requirement.
    - Added PSoC6 to topindex templates.
    
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinxcontrib.jquery',
+    'sphinx_tabs.tabs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
