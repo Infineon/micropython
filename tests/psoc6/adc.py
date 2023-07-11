fc95866575774ec0641cdecbfd3af7aa3a175cfe
NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>
2023-07-11 18:58:26 +0530

    tests/psoc6: Making tests to work right.
    Signed-off-by: NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>

--- tests/psoc6/adc.py
+++ tests/psoc6/adc.py
@@ -7,17 +7,14 @@ from machine import ADC, Pin
 adc_pin = Pin("P10_0")
 adc_wrong_pin = Pin("P13_7")
 
-adc = ADC(adc_pin)
-print(adc)
-
-adc = ADC(adc_pin, sample_ns=1200)
-print(adc)
-
-print(adc.read_uv() > 0)
-print(adc.read_u16() > 0)
-
 # Exceptions should be raised
 try:
     adc = ADC(adc_wrong_pin)
 except:
     print("Invalid ADC Pin")
+
+adc = ADC(adc_pin, sample_ns=1000)
+print(adc)
+
+print(adc.read_u16() > 0)
+print(adc.read_uv() > 0)
