c45b4658703bb1e0f9e83b9b451d33d005246f84
enriquezgarc <enriquezgarcia.external@infineon.com>
2023-07-25 16:05:20 +0200

    ports/psoc6/modules/machine/machine_adc: WIP refactor relationship.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- ports/psoc6/modules/machine/machine_adc.h
+++ ports/psoc6/modules/machine/machine_adc.h
@@ -8,9 +8,7 @@ typedef struct _machine_adc_obj_t {
     machine_adcblock_obj_t *block;
     uint32_t pin;
     uint32_t sample_ns;
-    cyhal_adc_channel_t *adc_chan_obj;
+    cyhal_adc_channel_t adc_chan_obj;
 } machine_adc_obj_t;
-
-extern machine_adc_obj_t *adc_init_helper(uint32_t sampling_time, uint32_t pin1, uint8_t bits);
 
 #endif // MICROPY_INCLUDED_MACHINE_ADC_H
