c7b66755b3104142a66f9922117595bfd2501e1c
enriquezgarc <enriquezgarcia.external@infineon.com>
2023-11-21 12:32:21 +0100

    docs: Modified docs conf to changes in RTD.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- docs/conf.py
+++ docs/conf.py
@@ -130,9 +130,15 @@ rst_epilog = """
 
 # -- Options for HTML output ----------------------------------------------
 
-import sphinx_rtd_theme
+# on_rtd is whether we are on readthedocs.org
+try:
+    import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), "."]
+except:
+    html_theme = "default"
+    html_theme_path = ["."]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
