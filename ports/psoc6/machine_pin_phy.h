8e650f4e27213a5a476f1e297ef05775f04eca9e
enriquezgarc <enriquezgarcia.external@infineon.com>
2024-07-19 11:38:34 +0200

    ports/psoc6/machine_pin_phy.h: Fixed typo.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- ports/psoc6/machine_pin_phy.h
+++ ports/psoc6/machine_pin_phy.h
@@ -15,7 +15,7 @@
             case PIN_PHY_I2C_IN_USE_ERROR: \
             case PIN_PHY_SPI_IN_USE_ERROR: \
             case PIN_PHY_UART_IN_USE_ERROR: \
-                mp_raise_msg(&mp_type_Exception, MP_ERROR_TEXT("The chosen pin(s) might be already in used by other machine instance.")); \
+                mp_raise_msg(&mp_type_Exception, MP_ERROR_TEXT("The chosen pin(s) might be already in use by another machine instance.")); \
                 break; \
         } \
 }
