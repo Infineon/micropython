f7c3071f7c0055c85dffeeaa0caf9f88d0b1e756
NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>
2024-07-23 13:43:58 +0530

    tests/ports/psoc6: Refactor PWM tests.
    Signed-off-by: NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>

--- tests/ports/psoc6/hw_ext/pwm.py
+++ tests/ports/psoc6/hw_ext/pwm.py
@@ -8,8 +8,8 @@ import time
 
 # TODO: This tests is failing too often. Enable it after proper review of timing, setup() teardown of resources.
 # Maybe the blocking implementation is not the most convenient. Interrupt based?
-print("SKIP")
-raise SystemExit
+# print("SKIP")
+# raise SystemExit
 
 # Allocate pin based on board
 board = os.uname().machine
@@ -20,47 +20,16 @@ elif "CY8CPROTO-063-BLE" in board:
     pwm_pin = "P12_6"
     pin_in = "P12_7"
 elif "CY8CKIT-062S2-AI" in board:
-    pwm_pin = "P9_6"
-    pin_in = "P9_7"
+    pwm_pin = "P9_0"
+    pin_in = "P9_1"
 
 input_pin = Pin(pin_in, Pin.IN)
 
 start_time = 0
 low_signal_start_time = 0
 high_signal_start_time = 0
-tolerance = 3.0
-duty_tolerance = 5.0
-debug = False
-
-
-def _print_val(params_list, print_list=False):
-    if print_list:
-        for params in params_list:
-            print(f"{params[0]} = {params[1]}")
-
-
-# def measure_signal_recursive():
-#     global start_time
-#     if input_pin.value() == 1:
-#         start_time = time.ticks_us()
-#         wait_for_low()
-#         return
-#     measure_signal_recursive()
-
-
-# def wait_for_low():
-#     global low_signal_start_time
-#     while input_pin.value():
-#         pass
-#     low_signal_start_time = time.ticks_us()
-#     wait_for_high()
-
-
-# def wait_for_high():
-#     global high_signal_start_time
-#     while input_pin.value() < 1:
-#         pass
-#     high_signal_start_time = time.ticks_us()
+tolerance = 8.0
+duty_tolerance = 8.0
 
 
 def measure_signal():
@@ -85,35 +54,43 @@ def validate_signal(exp_freq=0, exp_duty_u16=0, exp_duty_ns=0, exp_dutycycle=0):
     on_time = time.ticks_diff(low_signal_start_time, start_time)
     off_time = time.ticks_diff(high_signal_start_time, low_signal_start_time)
     time_period = on_time + off_time
-    freq = 1000000 / (time_period)
+    calc_freq = 1000000 / (time_period)
     dc = on_time / (time.ticks_diff(high_signal_start_time, start_time)) * 100
 
-    params = [
-        ["on_time(us)", on_time],
-        ["off_time(us)", off_time],
-        ["time_period(us)", time_period],
-        ["freq(Hz)", freq],
-        ["dc(%)", dc],
-    ]
-    _print_val(params, debug)
-
-    print(
-        "\nExpected freq(Hz) approx same as set freq(Hz) and experimentally calculated freq(Hz): ",
-        (exp_freq - tolerance) < pwm.freq() < (exp_freq + tolerance)
-        and (exp_freq - tolerance) < freq < (exp_freq + tolerance),
-    )
+    set_freq = pwm.freq()
+    set_duty_u16 = 0
+    set_duty_ns = 0
 
     if exp_duty_ns:
-        print("Dutycycle(ns) value is same as set value: ", pwm.duty_ns() == exp_duty_ns)
+        set_duty_ns = pwm.duty_ns()
     if exp_duty_u16:
-        print("Dutycycle(raw) value is same as set value: ", pwm.duty_u16() == exp_duty_u16)
+        set_duty_u16 = pwm.duty_u16()
 
-    print(
-        "Expected duty cycle(%) approx same as experimental duty cycle(%): ",
-        (exp_dutycycle - duty_tolerance) < dc < (exp_dutycycle + duty_tolerance),
-    )
+    if ((exp_freq - tolerance) < set_freq < (exp_freq + tolerance)) == False:
+        print(f"Exp freq does not match set freq! \n Exp freq: {exp_freq} \n Set freq: {set_freq}")
+
+    if ((exp_freq - tolerance) < calc_freq < (exp_freq + tolerance)) == False:
+        print(
+            f"Exp freq does not match calc freq! \n Exp freq: {exp_freq} \n Set freq: {calc_freq}"
+        )
+
+    if ((exp_dutycycle - duty_tolerance) < dc < (exp_dutycycle + duty_tolerance)) == False:
+        print(f"Exp dc(%) does not match calc dc(%)! \n Exp dc: {exp_dutycycle} \n Calc dc: {dc}")
+
+    if set_duty_ns != 0:
+        if set_duty_ns != exp_duty_ns:
+            print(
+                f"Exp dc(ns) does not match set dc(ns) \n Exp dc: {exp_duty_ns} \n Set dc: {set_duty_ns}"
+            )
+
+    if set_duty_u16 != 0:
+        if set_duty_u16 != exp_duty_u16:
+            print(
+                f"Exp dc(raw) does not match set dc(raw) \n Exp dc: {exp_duty_u16} \n Set dc: {set_duty_u16}"
+            )
 
 
+print("*** PWM tests ***")
 # T = 1sec (25% dc)
 pwm = PWM(pwm_pin, freq=1, duty_ns=250000000)
 # Let the first pulse pass
