a51bb83bb5bef311d272bec0671f2da381853a60
NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>
2024-07-23 16:14:19 +0530

    tests/ports/psoc6: Add print-failures tag.
    Signed-off-by: NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>

--- tests/ports/psoc6/run_psoc6_tests.sh
+++ tests/ports/psoc6/run_psoc6_tests.sh
@@ -143,7 +143,7 @@ run_tests() {
   test_dir_flag="-d"
   case ${tests} in *.py)  test_dir_flag="";; esac
 
-  ./run-tests.py --target psoc6 --device ${test_dev} ${test_dir_flag} ${tests} ${excluded_tests}
+  ./run-tests.py --print-failures --target psoc6 --device ${test_dev} ${test_dir_flag} ${tests} ${excluded_tests}
 
   update_test_result $?
 }
