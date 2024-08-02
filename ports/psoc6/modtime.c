256a89feb1b54116c0003934908c1daf1245776b
enriquezgarc <enriquezgarcia.external@infineon.com>
2024-08-02 13:26:55 +0200

    ports/psoc6: Added missing MIT License header and copyright notice.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- ports/psoc6/modtime.c
+++ ports/psoc6/modtime.c
@@ -4,7 +4,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2013-2021 Damien P. George
- * Copyright (c) 2023 Infineon Technologies
+ * Copyright (c) 2023-2024 Infineon Technologies AG
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
