7e562d704b6a70fcb9d70372213c8bb12ba98b20
NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>
2024-08-06 16:11:19 +0530

    tests/ports/psoc6: WDT refactor.
    Signed-off-by: NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>

--- tests/ports/psoc6/mp_custom/wdt.py
+++ tests/ports/psoc6/mp_custom/wdt.py
@@ -1,8 +1,8 @@
-import machine
+"""import machine
 import time
 
 # invalid test cases
-"""print("\n***** Test 1: Wrong WDT id *****\n")
+print("\n***** Test 1: Wrong WDT id *****\n")
 try:
     wdt = machine.WDT(1, timeout=6000)
 except Exception:
@@ -47,10 +47,39 @@ try:
 except Exception:
     print("FAIL")"""
 
-print("\n***** Test 4: WDT created successfully 6s *****\n")
-wdt = machine.WDT(id=0, timeout=1000)
-print(wdt)
-print("PASS")
-time.sleep_ms(1200)
-print("Sleep-over")
-# print(machine.reset_cause())
+
+import machine
+import time
+
+# invalid test cases
+print("\nTest 1: Wrong WDT id executing...")
+try:
+    wdt = machine.WDT(1, timeout=6000)
+    print("Test 1 failed")
+except Exception:
+    pass
+
+print("Test 2: WDT timeout too low executing...")
+try:
+    wdt = machine.WDT(0, 0)
+    print("Test 2 failed")
+except Exception:
+    pass
+
+print("Test 3: WDT timeout too high executing...")
+try:
+    wdt = machine.WDT(id=0, timeout=6001)
+    print("Test 3 failed")
+except Exception:
+    pass
+
+# valid test cases
+
+print("Test 4: Rest cause check executing...")
+try:
+    wdt = machine.WDT(id=0, timeout=500)
+except Exception:
+    print("WDT instance creation failed!")
+
+time.sleep_ms(500)
+print("Sleep over")
