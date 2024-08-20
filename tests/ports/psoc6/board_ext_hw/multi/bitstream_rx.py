e153615f113c17cad1395bf21cf97137d15fbcbc
IFX-Anusha <Anusha.TR@infineon.com>
2024-08-20 11:57:30 +0530

    tests/psoc6: Bitstream Pin change.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- tests/ports/psoc6/board_ext_hw/multi/bitstream_rx.py
+++ tests/ports/psoc6/board_ext_hw/multi/bitstream_rx.py
@@ -13,12 +13,12 @@ another is configured as an input, which is used for receiving the bitstream sig
 board = os.uname().machine
 if "CY8CPROTO-062-4343W" in board:
     bitstream_in_pin_name = "P12_1"
-    rx_ready_signal_pin_name = "P13_4"
+    rx_ready_signal_pin_name = "P12_3"
 elif "CY8CPROTO-063-BLE" in board:
     bitstream_in_pin_name = "P5_2"
     rx_ready_signal_pin_name = "P6_2"
 elif "CY8CKIT-062S2-AI" in board:
-    bitstream_in_pin_name = "P9_6"
+    bitstream_in_pin_name = "P9_3"
     rx_ready_signal_pin_name = "P9_7"
 
 expected_values = [
