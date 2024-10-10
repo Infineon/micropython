35c8e1443a9cbee6479574cbf74570e74c0f4317
enriquezgarc <enriquezgarcia.external@infineon.com>
2024-10-10 16:20:08 +0200

    tests/ports/psoc6/../timer.py: Added workaround for BLE-063 board.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- tests/ports/psoc6/board_only_hw/single/timer.py
+++ tests/ports/psoc6/board_only_hw/single/timer.py
@@ -1,3 +1,4 @@
+import os
 from machine import Timer
 import time
 
@@ -76,3 +77,11 @@ if __name__ == "__main__":
     test_periodic()
     print("*****Multiple Timers Execution*****")
     test_multiple_timers()
+
+    # TODO: Timer tests need to be refined.
+    # The current implementation is not reliable.
+    # After this tests CY8CPROTO-063-BLE board is not able to the subsequent test.
+    # This workaround works for
+    board = os.uname().machine
+    if "CY8CPROTO-063-BLE" in board:
+        time.sleep(3)
