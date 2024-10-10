3b1b9332e04161a66f4158c38fe21d62c6dbd609
enriquezgarc <enriquezgarcia.external@infineon.com>
2024-10-10 13:18:17 +0200

    docs/conf.py: Removed get_html_theme_path for rtd conf.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- docs/conf.py
+++ docs/conf.py
@@ -131,7 +131,7 @@ try:
     import sphinx_rtd_theme
 
     html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), "."]
+    html_theme_path = ["."]
 except:
     html_theme = "default"
     html_theme_path = ["."]
