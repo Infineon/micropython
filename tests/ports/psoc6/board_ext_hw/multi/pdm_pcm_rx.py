3a3266cf44e7ef542dc3a94d1ab85b65eeb7d5dc
NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>
2024-11-26 14:55:32 +0530

    tests/ports/psoc6: Test PDM works.
    Signed-off-by: NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>

--- tests/ports/psoc6/board_ext_hw/multi/pdm_pcm_rx.py
+++ tests/ports/psoc6/board_ext_hw/multi/pdm_pcm_rx.py
@@ -6,6 +6,7 @@ import time
 # Allocate pin based on board
 board = os.uname().machine
 if "CY8CPROTO-062-4343W" in board:
+    print("Board selected")
     clk_pin = "P10_4"
     data_pin = "P10_5"
     send_signal_to_tx_pin = "P10_3"
