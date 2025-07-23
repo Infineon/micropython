66570666d35a085d34d8b762e84d9af71fbe7626
NikhitaR-IFX <nikhita.rajasekhar@infineon.com>
2025-07-23 15:48:33 +0530

    tests/ports/psoc6: Refactor time_pulse_us test.
    Signed-off-by: NikhitaR-IFX <nikhita.rajasekhar@infineon.com>

--- tests/ports/psoc6/board_ext_hw/multi/time_pulse_sig_gen.py
+++ tests/ports/psoc6/board_ext_hw/multi/time_pulse_sig_gen.py
@@ -5,14 +5,14 @@ import time
 # Allocate pin based on board
 board = os.uname().machine
 if "CY8CPROTO-062-4343W" in board:
-    pulse_out_pin = "P9_7"
-    ack_in_pin = "P9_6"
+    pulse_out_pin = "P12_1"
+    ack_in_pin = "P12_3"
 elif "CY8CPROTO-063-BLE" in board:
-    pulse_out_pin = "P9_4"
-    ack_in_pin = "P9_6"
+    pulse_out_pin = "P5_2"
+    ack_in_pin = "P6_2"
 elif "CY8CKIT-062S2-AI" in board:
-    pulse_out_pin = "P9_7"
-    ack_in_pin = "P9_6"
+    pulse_out_pin = "P9_5"
+    ack_in_pin = "P9_7"
 
 ack_recvd = False
 
@@ -28,28 +28,26 @@ def blocking_delay_ms(delay_ms):
         pass
 
 
-pulse_out = Pin(pulse_out_pin, Pin.OUT, value=False)
+pulse_out = Pin(pulse_out_pin, Pin.OUT)
 ack_in = Pin(ack_in_pin, Pin.IN)
 ack_in.irq(trigger=Pin.IRQ_RISING, handler=cback)
 
 # Starting condition always low pulse
 pulse_out.low()
-blocking_delay_ms(500)
 
 while not ack_recvd:
     pass  # Wait for the Begin ACK signal
 
-ack_recvd = False
+blocking_delay_ms(100)
 
 # Generate high pulse
 pulse_out.high()
-while not ack_recvd:
-    pass  # Wait for high received ACK signal
 
 # Wait for 1sec to time the pulse
 blocking_delay_ms(1000)
 
 pulse_out.low()
 
-pulse_out.deinit()  # Deinitialize the pulse output pin
+# Deinitialize the pins
+pulse_out.deinit()
 ack_in.deinit()
