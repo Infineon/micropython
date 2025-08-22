87add2175e0521745e4fd670194175a4d70e582d
NikhitaR-IFX <nikhita.rajasekhar@infineon.com>
2025-08-22 12:51:01 +0530

    psoc6: Extend modbluetooth from extmod.
    Signed-off-by: NikhitaR-IFX <nikhita.rajasekhar@infineon.com>

--- ports/psoc6/modbluetooth.c
+++ ports/psoc6/modbluetooth.c
@@ -10,55 +10,129 @@
 #include "py/runtime.h"
 
 
-#include "ble/cycfg_bt_settings.h"
+#include "cycfg_bt_settings.h"
 #include "modbluetooth.h"
 
-#if MICROPY_PSOC6_BLUETOOTH
+#include "wiced_bt_stack.h"
+#include "cybsp.h"
+#include "cy_retarget_io.h"
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+#include <string.h>
+#include "cybt_platform_trace.h"
+#include "wiced_memory.h"
+#include "cyhal.h"
+#include "stdio.h"
+#include "cycfg_gatt_db.h"
+#include "cycfg_gap.h"
+#include "wiced_bt_dev.h"
+#include "cybsp_bt_config.h"
 
-// ----------------------------------------------------------------------------
-// Bluetooth object: General
-// ----------------------------------------------------------------------------
+#include "extmod/modbluetooth.h"
 
-static const mp_obj_type_t mp_type_bluetooth_ble;
+// ******************************************************************************
+#if MICROPY_PY_BLUETOOTH
+wiced_result_t app_bt_management_callback(wiced_bt_management_evt_t event,
+    wiced_bt_management_evt_data_t *p_event_data) {
 
-static mp_obj_t bluetooth_ble_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
-    mp_printf(&mp_plat_print, "To be implemented\n");
-    return mp_const_none;
+    return WICED_BT_SUCCESS;
+
 }
 
+// extern const cybt_platform_config_t cybsp_bt_platform_cfg;
+int mp_bluetooth_init(void) {
+    // Initialize the Bluetooth hardware and stack.
+    mp_printf(&mp_plat_print, "mp_bluetooth_init invoked \n");
 
-// ----------------------------------------------------------------------------
-// Bluetooth object: Definition
-// ----------------------------------------------------------------------------
+    return 0; // Return 0 on success.
 
-static const mp_rom_map_elem_t bluetooth_ble_locals_dict_table[] = {
-    // General
-};
-static MP_DEFINE_CONST_DICT(bluetooth_ble_locals_dict, bluetooth_ble_locals_dict_table);
+}
 
-static MP_DEFINE_CONST_OBJ_TYPE(
-    mp_type_bluetooth_ble,
-    MP_QSTR_BLE,
-    MP_TYPE_FLAG_NONE,
-    make_new, bluetooth_ble_make_new,
-    locals_dict, &bluetooth_ble_locals_dict
-    );
+int mp_bluetooth_deinit() {
+    return 0;
+}
 
+// Returns true when the Bluetooth stack is active.
+bool mp_bluetooth_is_active(void) {
+    return false;
+}
 
-static const mp_rom_map_elem_t mp_module_bluetooth_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_bluetooth) },
-    { MP_ROM_QSTR(MP_QSTR_BLE), MP_ROM_PTR(&mp_type_bluetooth_ble) },
-};
+// Gets the current address of this device in big-endian format.
+void mp_bluetooth_get_current_address(uint8_t *addr_type, uint8_t *addr) {
 
-static MP_DEFINE_CONST_DICT(mp_module_bluetooth_globals, mp_module_bluetooth_globals_table);
+}
 
-const mp_obj_module_t mp_module_bluetooth = {
-    .base = { &mp_type_module },
-    .globals = (mp_obj_dict_t *)&mp_module_bluetooth_globals,
-};
+// Sets the addressing mode to use.
+void mp_bluetooth_set_address_mode(uint8_t addr_mode) {
 
-MP_REGISTER_EXTENSIBLE_MODULE(MP_QSTR_bluetooth, mp_module_bluetooth);
+}
 
-MP_REGISTER_ROOT_POINTER(mp_obj_t bluetooth);
+// Get or set the GAP device name that will be used by service 0x1800, characteristic 0x2a00.
+size_t mp_bluetooth_gap_get_device_name(const uint8_t **buf) {
+    return 0;
+}
+
+int mp_bluetooth_gap_set_device_name(const uint8_t *buf, size_t len) {
+    return 0;
+}
+
+// Start advertisement. Will re-start advertisement when already enabled.
+// Returns errno on failure.
+int mp_bluetooth_gap_advertise_start(bool connectable, int32_t interval_us, const uint8_t *adv_data, size_t adv_data_len, const uint8_t *sr_data, size_t sr_data_len) {
+    return 0;
+}
+
+// Stop advertisement. No-op when already stopped.
+void mp_bluetooth_gap_advertise_stop(void) {
+    return;
+}
+
+// Start adding services. Must be called before mp_bluetooth_register_service.
+int mp_bluetooth_gatts_register_service_begin(bool append) {
+    return 0;
+}
+// Add a service with the given list of characteristics to the queue to be registered.
+// The value_handles won't be valid until after mp_bluetooth_register_service_end is called.
+int mp_bluetooth_gatts_register_service(mp_obj_bluetooth_uuid_t *service_uuid, mp_obj_bluetooth_uuid_t **characteristic_uuids, uint16_t *characteristic_flags, mp_obj_bluetooth_uuid_t **descriptor_uuids, uint16_t *descriptor_flags, uint8_t *num_descriptors, uint16_t *handles, size_t num_characteristics) {
+    return 0;
+}
+// Register any queued services.
+int mp_bluetooth_gatts_register_service_end(void) {
+    return 0;
+}
+
+// Read the value from the local gatts db (likely this has been written by a central).
+int mp_bluetooth_gatts_read(uint16_t value_handle, const uint8_t **value, size_t *value_len) {
+    return 0;
+}
+// Write a value to the local gatts db (ready to be queried by a central). Optionally send notifications/indications.
+int mp_bluetooth_gatts_write(uint16_t value_handle, const uint8_t *value, size_t value_len, bool send_update) {
+    return 0;
+}
+// Send a notification/indication to the central, optionally with custom payload (otherwise the DB value is used).
+int mp_bluetooth_gatts_notify_indicate(uint16_t conn_handle, uint16_t value_handle, int gatts_op, const uint8_t *value, size_t value_len) {
+    return 0;
+}
+
+// Resize and enable/disable append-mode on a value.
+// Append-mode means that remote writes will append and local reads will clear after reading.
+int mp_bluetooth_gatts_set_buffer(uint16_t value_handle, size_t len, bool append) {
+    return 0;
+}
+
+// Disconnect from a central or peripheral.
+int mp_bluetooth_gap_disconnect(uint16_t conn_handle) {
+    return 0;
+}
+
+// Set/get the MTU that we will respond to a MTU exchange with.
+int mp_bluetooth_get_preferred_mtu(void) {
+    return 0;
+}
+
+int mp_bluetooth_set_preferred_mtu(uint16_t mtu) {
+    return 0;
+}
 
 #endif
