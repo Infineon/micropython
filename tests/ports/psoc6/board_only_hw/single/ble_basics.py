28af921c183f9a6503b542c82d43f57f7a35cf98
NikhitaR-IFX <nikhita.rajasekhar@infineon.com>
2025-09-11 16:07:37 +0530

    psoc6/modbluetooth: Add config() and en irq().
    Signed-off-by: NikhitaR-IFX <nikhita.rajasekhar@infineon.com>

--- tests/ports/psoc6/board_only_hw/single/ble_basics.py
+++ tests/ports/psoc6/board_only_hw/single/ble_basics.py
@@ -11,10 +11,28 @@ elif "CY8CKIT-062S2-AI" in board:
     raise SystemExit
 
 
+def irq_handle(event, data):
+    pass
+
+
 ble_obj = bluetooth.BLE()
 ble_obj.active(True)
 print("BLE active: ", ble_obj.active())
 
-print("Turning off BLE radio")
+# Config get test
+print("\n** Configurations set for device **")
+print("GAP_NAME: ", ble_obj.config("gap_name"))
+print("MTU: ", ble_obj.config("mtu"))
+
+ble_obj.config(addr_mode=0)  # Public
+print("MAC: ", ble_obj.config("mac")[1].hex())
+
+# Check if ble.irq registers handle
+ble_obj.irq(irq_handle)
+
+# Cannot be verified since trng is used and changes in every test
+# ble_obj.config(addr_mode=1) #Static Random
+# print("Set random address: ", ble_obj.config("mac")[1].hex())
+print("\n")
 ble_obj.active(False)
-print("BLE active: ", ble_obj.active())
+print("Turned off BLE radio: ", not ble_obj.active())
