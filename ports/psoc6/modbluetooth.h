c923c3889aa8e4047b1f291fdfed9fbdcb074f3a
NikhitaR-IFX <nikhita.rajasekhar@infineon.com>
2025-10-13 11:13:55 +0530

    psoc6: Enabled GAP advertising mode.
    Signed-off-by: NikhitaR-IFX <nikhita.rajasekhar@infineon.com>

--- ports/psoc6/modbluetooth.h
+++ ports/psoc6/modbluetooth.h
@@ -7,6 +7,8 @@
 #include "py/objlist.h"
 #include "py/ringbuf.h"
 
+#include "wiced_bt_ble.h"
+
 // ToDo: Micropython BLE states - Does this match with PSOC BLE Stack?
 enum {
     MP_BLUETOOTH_BLE_STATE_OFF,
@@ -22,11 +24,11 @@ extern volatile int mp_bluetooth_ble_stack_state;
         mp_raise_msg_varg(&mp_type_ValueError, MP_ERROR_TEXT(msg), ret); \
 }
 
+
 // Port specific configuration.
 #ifndef MICROPY_PY_BLUETOOTH_RINGBUF_SIZE
 #define MICROPY_PY_BLUETOOTH_RINGBUF_SIZE (128)
 #endif
-
 
 
 #endif // MICROPY_INCLUDED_PSOC6_MODBLUETOOTH_H
